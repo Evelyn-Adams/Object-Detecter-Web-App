6f39511dbb0b3167cd1d880afd9c8d508d8ac927
Evelyn-Adams <94699970+Evelyn-Adams@users.noreply.github.com>
2022-02-12 21:29:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function ABCD(){
+window.location="BedRoom.html";
+}
+function BCDE(){
+window.location="Bottle.html";
+}
+function CDEF(){
+window.location="Fruits.html";
+}
+function DEFG(){
+window.location="Desk.html";
+}
+function EEFGH(){
+window.location='Tv&Ac.html';
+}
